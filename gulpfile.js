79c2cdb79fce7d7f1c5de775271fd1c4f3295101
Klaudia Przespolewska <klaudia1000@onet.eu>
2017-01-07 16:49:09 +0100

    dodatki

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+var gulp=require('gulp'); //tworzy środowisko gulpa
+var sass=require('gulp-sass');
+
+var browserSync=require('browser-sync').create();
+
+gulp.task('browserSync', function(){
+    browserSync.init ({
+        server :{
+        baseDir: "app"
+    },
+    })
+})
+
+
+
+//na obiekcie gulpa wykonuje konkretna funkcje. Ma skompilowac pliki do sassa
+gulp.task('sass', function (){
+    return gulp.src('app/scss/**/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('app/css'))
+    .pipe(browserSync.reload({stream: true
+    }))
+});
